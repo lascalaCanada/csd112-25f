a1328b7804f8702acc60d0c40ffdc56ba074431c
Fernando Lascala <24057211@saultcollege.ca>
2025-11-05 00:32:00 -0500

    CSD112 - 25F - Lab #7 - CSS Layout - v01

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,10 @@
 window.addEventListener('load', () => {
-  let name = prompt("Please enter your name!"); 
-  if ( ! name ) { return; }
-  let headingElement = document.querySelector('h1'); 
-  let heading = headingElement.innerHTML; 
-  heading = heading.substring(0, heading.length - 1);   
-  heading = heading + ", " + name + "!"; 
-  headingElement.innerHTML = heading; 
+    let name = prompt("Please enter your name!"); 
+    if ( ! name ) { return; }
+    let headingElement = document.querySelector('h1'); 
+    let heading = headingElement.innerHTML; 
+    heading = heading.substring(0, heading.length - 1);   
+    heading = heading + ", " + name + "!"; 
+    headingElement.innerHTML = heading; 
 });
+
